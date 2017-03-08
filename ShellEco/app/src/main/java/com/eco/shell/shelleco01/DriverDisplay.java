ef9b70e64a7f44bef678e3cfaf95c484309f612c
SpydaBeast <brien@artofmars.org>
2017-03-08 14:03:18 -0500

    adding Shelleco project to repo. working IU. Need to have location broadcast to display most recently location.

---
+++ ShellEco/app/src/main/java/com/eco/shell/shelleco01/DriverDisplay.java
@@ -0,0 +1,113 @@
+package com.eco.shell.shelleco01;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+public class DriverDisplay extends AppCompatActivity {
+
+    private Button btnStart, btnStop;
+    private TextView navDisplay, speedData;
+    private BroadcastReceiver broadcastReceiver;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (broadcastReceiver == null){
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+
+                        navDisplay.append("" + intent.getExtras().get("coordinates"));
+                        //speedData.append("" + intent.getExtras().get("speed"));
+                        //navDisplay.getEditableText().clear();
+
+                }
+
+        };
+        }
+        registerReceiver(broadcastReceiver, new IntentFilter("Location_Update"));
+        registerReceiver(broadcastReceiver, new IntentFilter("Speed_Update"));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(broadcastReceiver != null){
+            unregisterReceiver(broadcastReceiver);
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_display);
+
+        btnStart = (Button) findViewById(R.id.btn_start);
+        btnStop = (Button) findViewById(R.id.btn_Stop);
+        navDisplay = (TextView) findViewById(R.id.nav_data);
+        speedData = (TextView) findViewById(R.id.speed_data);
+
+        if(!runtime_permissions())
+            enable_button();
+    }
+
+    private void enable_button() {
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), GPS_Service.class);
+                startService(i);
+
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), GPS_Service.class);
+                stopService(i);
+
+            }
+        });
+    }
+
+    private boolean runtime_permissions() {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 100){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                enable_button();
+            }else{
+                runtime_permissions();
+            }
+        }
+    }
+}
---
+++ ShellEco/app/src/main/java/com/eco/shell/shelleco01/GPS_Service.java
@@ -0,0 +1,81 @@
+package com.eco.shell.shelleco01;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Brien on 2/27/2017.
+ */
+
+public class GPS_Service extends Service {
+
+    private LocationListener listener;
+    private LocationManager locationManager;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                Intent i = new Intent("Location_Update");
+
+                i.putExtra("coordinates",location.getLatitude()+ " " +location.getLongitude()
+                );
+                Intent j = new Intent("Speed_Update");
+                i.putExtra("speed",location.getSpeed()
+                );
+                sendBroadcast(i);
+
+                //recently add line to remove coordinates
+                i.removeExtra("coordinates");
+            }
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                //noinspection MissingPermission
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+
+            }
+        };
+        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+
+        //noinspection MissingPermission
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,0,listener);
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(locationManager != null){
+            //noinspection MissingPermission
+            locationManager.removeUpdates(listener);
+
+        }
+    }
+}
